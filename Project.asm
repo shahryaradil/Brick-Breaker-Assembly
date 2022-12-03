581b4cdac43420dd82803d4a23a2294f499ff82f
SMuhammadMustafa <mmustafa1444@gmail.com>
2022-12-03 07:01:50 +0500

    feat: Add ball, movement to ball, collision of ball with walls, collision of ball with bricks (70% done)

--- Project.asm
+++ Project.asm
@@ -1,8 +1,16 @@
 include Brick.inc
+include Ball.inc
 .model small
 .stack 0100h
 .data
 
+currTime db ?
+isInBrickX db 0
+rowsOfBricks dw 3
+colsOfBricks dw 6
+numberOfBricks dw 18
+ballSpeedX dw 3
+ballSpeedY dw 3
 brickLength dw 10
 brickWidth dw 30
 brickStartX dw 40
@@ -22,6 +30,8 @@ defaultSelect db 1
 
 bricks Brick 18 dup (<?,?,10,30,1,1,14>)
 brick1 Brick <?,?,10,30,1,1,14>
+
+balls Ball <150, 185, 1, -1, 1, 5, 5, 1>
 
 .code
 
@@ -122,6 +132,37 @@ pageUpdate macro pNo ;change page number
 
 	mov al, 14
 	mov bh, pNo
+
+endm
+
+drawFrame macro
+
+	local timeLoop
+	
+	push cx
+	push dx
+
+	timeLoop:
+		
+		mov ah, 2ch
+		int 21h
+
+		movePaddle	; move paddle left or right
+
+		cmp dl, currTime
+		je timeLoop
+
+		mov currTime, dl
+
+		drawrect balls.lengthOfBall,balls.widthOfBall,balls.x,balls.y,0
+		moveBallX
+		moveBallY
+		drawrect balls.lengthOfBall,balls.widthOfBall,balls.x,balls.y,15
+
+		jmp timeLoop
+
+	pop dx
+	pop cx
 
 endm
 
@@ -230,12 +271,13 @@ mainScreenFunction macro
 	jmp top
 
 	startGame:
-		pageUpdate 1 ; startGame func to be added ;
-		drawGame 3,6
+		pageUpdate 1						; update page
+		drawGame rowsOfBricks,colsOfBricks	; draw start of game
+		drawFrame							; start drawing frames
 
 endm
 
-drawGame macro rows, cols, paddleCol
+drawGame macro rows, cols
 
 	local startBrickDrawOuter, startBrickDrawInner, contLoop	; set labels as local to avoid redefinition error
 
@@ -244,10 +286,6 @@ drawGame macro rows, cols, paddleCol
 	push ax
 	push dx
 	push bx
-
-	mov ah,0
-	mov al,13h
-	int 10h
 
 	mov brickStartX, 40
 	mov brickStartY, 50
@@ -266,7 +304,7 @@ drawGame macro rows, cols, paddleCol
 
 			mov al, 1
 			cmp [si + 6], al		; check if bricks.isActive is 1
-			jne contLoop			; only draw active bricks
+			jne contLoop			; only draw active bricks		; Code Check
 
 			drawrect brickLength,brickWidth,brickStartX,brickStartY,14
 			mov ax, brickStartX
@@ -285,7 +323,7 @@ drawGame macro rows, cols, paddleCol
 
 	loop startBrickDrawOuter
 
-	drawrect paddleLength,paddleWidth,paddleX,195,9
+	drawrect paddleLength,paddleWidth,paddleX,195,9				; Draw Paddle on initial position
 
 	pop bx							; pop all registers that were used
 	pop dx
@@ -295,38 +333,233 @@ drawGame macro rows, cols, paddleCol
 
 endm
 
+moveBallX macro
+
+	local increment, decrement, changeDirection, done, checkCollision1, checkCollision2, contLoop1, contLoop2, flagOn
+
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+
+	mov si, offset bricks
+	mov bx, type bricks
+	mov cx, numberOfBricks
+	mov isInBrickX, 0
+
+	cmp balls.xDir, 1
+	je increment
+	jmp decrement
+	
+	increment:
+		cmp balls.x, 315
+		jge changeDirection
+
+		mov ax, ballSpeedX
+		add balls.x, ax
+		
+		checkCollision1:
+
+			mov al, [si + 6]
+			cmp al, 0
+			je contLoop1
+
+			mov dx, [si]
+			cmp balls.x, dx
+			jl contLoop1
+
+			add dl, [si + 5]
+			cmp balls.x, dx
+			jl flagOn
+
+			contLoop1:
+			add si, bx
+
+		loop CheckCollision1
+
+		jmp done
+
+	decrement:
+		cmp balls.x, 0
+		jle changeDirection
+		
+		mov ax, ballSpeedX
+		sub balls.x, ax
+
+		checkCollision2:
+
+			mov al, [si + 6]
+			cmp al, 0
+			je contLoop2
+
+			mov dx, [si]
+			cmp balls.x, dx
+			jl contLoop2
+
+			add dl, [si + 5]
+			cmp balls.x, dx
+			jl flagOn
+
+			contLoop2:
+			add si, bx
+
+		loop CheckCollision2
+
+		jmp done
+
+	changeDirection:
+		neg balls.xDir
+		jmp done
+
+	flagOn:
+		mov isInBrickX, 1
+
+	done:
+
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+
+endm
+
+moveBallY macro
+
+	local increment, decrement, changeDirection, done, checkCollision1, checkCollision2, contLoop1, contLoop2, flagOn
+
+	push ax	
+	push bx
+	push cx
+	push dx
+
+	mov si, offset bricks
+	mov bx, type bricks
+	mov cx, numberOfBricks
+
+	cmp balls.yDir, 1
+	je increment
+	jmp decrement
+	
+	increment:
+		cmp balls.y, 195
+		jge changeDirection
+
+		mov ax, ballSpeedY
+		add balls.y, ax
+
+		cmp isInBrickX, 1
+		jne done
+
+		mov dx, [si + 2]
+		cmp balls.y, dx
+		jl done
+
+		add dx, [si + 4]
+		cmp balls.y, dx
+		jl flagOn
+
+		jmp done
+
+	decrement:
+		cmp balls.y, 0
+		jle changeDirection
+
+		mov ax, ballSpeedY
+		sub balls.y, ax
+
+		cmp isInBrickX, 1
+		jne done
+
+		mov dx, [si + 2]
+		cmp balls.y, dx
+		jl done
+
+		add dx, [si + 4]
+		cmp balls.y, dx
+		jl flagOn
+
+		jmp done
+
+	changeDirection:
+		neg balls.yDir
+		jmp done
+
+	flagOn:
+		mov al, 0
+		mov [si + 6], al
+		mov bx, [si]
+		mov cx, [si + 2]
+		;drawrect brickLength,brickWidth,bx,cx,4
+		jmp changeDirection
+
+	done:
+
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+
+endm
+
 movePaddle macro
 
-	top: ;infinite loop to take user input 
-		mov ah, 0
-		int 16h
-		cmp ah, 77
-		je up
-		cmp ah, 75
-		je down
-		cmp ah, 1
-		je exit
-	jmp top
-	jz top
+	local left, right, done
 
-	up: ;case validator in case of up key being pressed
+	push ax
+
+	mov ah, 1
+	int 16h
+
+	jz done
+
+	mov ah, 0
+	int 16h	
+
+	cmp ah, 77
+	je right
+	cmp ah, 75
+	je left
+	cmp ah, 1
+	je exit
+
+	right: ;case validator in case of up key being pressed
+
+		mov ax, paddleWidth									; if paddle is at the end of screen, don't move further
+		add ax, paddleX
+		cmp ax, 320
+		jge done
+
+		drawrect paddleLength,paddleWidth,paddleX,195,0		; draw black on current position
 		add paddleX, 5
-		drawGame 3, 6
-	jmp top
+		drawrect paddleLength,paddleWidth,paddleX,195,9		; draw new on updated position
 
-	down:
+	jmp done
+
+	left:
+
+		mov ax, paddleX										; if paddle is at the end of screen, don't move further
+		cmp ax, 0
+		jle done
+
+		drawrect paddleLength,paddleWidth,paddleX,195,0		; draw black on current position
 		sub paddleX, 5
-		drawGame 3, 6
-	jmp top
+		drawrect paddleLength,paddleWidth,paddleX,195,9		; draw new on updated position
+
+	jmp done
+
+	done:
+	pop ax
 
 endm
 
 main proc
 
-	pageUpdate 0 ;page number for main menu
+	pageUpdate 0		; page number for main menu
 
-	mainScreenDraw ;initialize main menu and draw all objects
-	mainScreenFunction ;function to take user input for choice selection
+	mainScreenDraw		; initialize main menu and draw all objects
+	mainScreenFunction	; function to take user input for choice selection
 	
 	movePaddle	
 
